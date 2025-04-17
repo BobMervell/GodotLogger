f35e7dc59d6ab5d86f935852b4d6ad84a91c7e3f
Bob_mervell <b0bm3r3ll@gmail.com>
2025-04-17 22:08:14 +0200

    fix: Removed code smells

--- addons/Godot_logger/logger_autoload.gd
+++ addons/Godot_logger/logger_autoload.gd
@@ -1,28 +1,28 @@
 @tool
 extends Node
 
-var config = ConfigFile.new()
+var config:ConfigFile = ConfigFile.new()
 
 func _ready() -> void:
 	var err = config.load("res://addons/Godot_logger/log_settings.cfg")
 	if err != OK:
 		return
 
-func log_info(log_message: String, log_details: Dictionary = {}):
+func log_info(log_message: String, log_details: Dictionary = {}) -> void:
 	if config.get_value("LogSettings","log_info",true):
 		var error_type:String = "[b]INFO:[/b] "
 		var output_text:String = format_log(log_message,log_details)
 
 		print_rich(error_type + output_text)
 
-func log_debug(log_message: String, log_details: Dictionary = {}):
+func log_debug(log_message: String, log_details: Dictionary = {}) -> void:
 	if config.get_value("LogSettings","log_debug",true):
 		var error_type:String = "[b][color=#3178C7]DEBUG:[/color][/b] "
 		var output_text:String = format_log(log_message,log_details)
 
 		print_rich(error_type + output_text)
 
-func log_warn(log_message: String, log_details: Dictionary = {}):
+func log_warn(log_message: String, log_details: Dictionary = {}) -> void:
 	if config.get_value("LogSettings","log_warn",true):
 		var error_type:String = "[b][color=#FCDD52]WARNING:[/color][/b] "
 		var output_text:String = format_log(log_message,log_details)
@@ -30,7 +30,7 @@ func log_warn(log_message: String, log_details: Dictionary = {}):
 		push_warning(output_text)
 		print_rich(error_type + output_text)
 
-func log_error(log_message: String, log_details: Dictionary = {}):
+func log_error(log_message: String, log_details: Dictionary = {}) -> void:
 	if config.get_value("LogSettings","log_error",true):
 		var error_type:String = "[b][color=#F5693D]ERROR:[/color][/b] "
 		var output_text:String = format_log(log_message,log_details)
@@ -38,7 +38,7 @@ func log_error(log_message: String, log_details: Dictionary = {}):
 		push_error(output_text)
 		print_rich(error_type + output_text)
 
-func log_fatal(log_message: String, log_details: Dictionary = {}):
+func log_fatal(log_message: String, log_details: Dictionary = {}) -> void:
 	if config.get_value("LogSettings","log_fatal",true):
 		var error_type:String = "[b][color=#CB3D3D]FATAL:[/color][/b] "
 		var output_text:String = format_log(log_message,log_details)
